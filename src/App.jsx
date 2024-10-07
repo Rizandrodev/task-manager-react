ab406bd58e18224deb3ef586bc78536573eafa3c
Your Name <you@example.com>
2024-10-07 14:03:49 +0100

    componente task

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
       isCompleted: false
     }
   ]);
+
   function onTaskClick(taskID) {
    const updatedTasks = tasks.map((task) => {
      // Atualizar a tarefa se o ID corresponder
@@ -28,13 +29,20 @@ function App() {
  
    setTasks(updatedTasks);
  }
+
+ function onDeleteTaskClick(taskID){
+      const newTAsk=tasks.filter((task)=>task.id != taskID)
+      setTasks(newTAsk)
+ }
  
 
   return (
     <div className="w-screen h-screen bg-slate-400 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
-        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+        <Tasks tasks={tasks}
+         onTaskClick={onTaskClick}
+         onDeleteTaskClick={onDeleteTaskClick} />
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,19 +1,25 @@
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
 
-export function Tasks(props) {
+export function Tasks({tasks,onTaskClick,onDeleteTaskClick}) {
   return (
     <div>
       <ul className="space-y-4 p-6 bg-slate-200 rounded-md">
-        {props.tasks.map((task) => (
+        {tasks.map((task) => (
           <li className="flex gap-2" key={task.id}>
             <button
-              onClick={() => props.onTaskClick(task.id)}
+              onClick={() =>onTaskClick(task.id)}
               className={`bg-slate-400 w-full text-white p-2 rounded-md ${task.isCompleted ? "line-through" : ""}`}
             >
               {task.title}
             </button>
+
             <button className="bg-slate-400 text-white p-2 rounded-md">
               <ChevronRightIcon />
+            </button>
+            <button
+            onClick={()=>onDeleteTaskClick(task.id)}
+             className={`bg-slate-400 text-white p-2 rounded-md`}>
+              <TrashIcon />
             </button>
           </li>
         ))}

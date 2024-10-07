86a4d8a1bb51da45c18b96902b4bfb023a2b9ae5
Your Name <you@example.com>
2024-10-07 14:45:02 +0100

    Add task

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,61 @@
 import { useState } from "react";
 import { Tasks } from "./components/Tasks";
+import { AddTasks } from "./components/AddTasks";
 
 function App() {
   const [tasks, setTasks] = useState([
     {
       id: 1,
       title: "Study programming",
-      description: "Study programming until you become a full stack developer", // Corrigido 'description'
+      description: "Study programming until you become a full stack developer",
       isCompleted: false
     },
     {
       id: 2,
       title: "Study Pentest",
-      description: "Studying pentest to improve security skills", // Corrigido 'description'
+      description: "Studying pentest to improve security skills",
       isCompleted: false
     }
   ]);
 
   function onTaskClick(taskID) {
-   const updatedTasks = tasks.map((task) => {
-     // Atualizar a tarefa se o ID corresponder
-     if (task.id === taskID) {
-       return { ...task, isCompleted: !task.isCompleted };
-     }
-     // Retornar a tarefa sem modificações
-     return task;
-   });
- 
-   setTasks(updatedTasks);
- }
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === taskID) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
 
- function onDeleteTaskClick(taskID){
-      const newTAsk=tasks.filter((task)=>task.id != taskID)
-      setTasks(newTAsk)
- }
- 
+    setTasks(updatedTasks);
+  }
+
+  function onDeleteTaskClick(taskID) {
+    const newTasks = tasks.filter((task) => task.id !== taskID);
+    setTasks(newTasks);
+  }
+
+  function onAddtaskSubmitt(title, description) {
+    const newTask = {
+      id: tasks.length + 1,
+      title,
+      description,
+      isCompleted: false
+    };
+
+    setTasks([...tasks, newTask]); 
+  }
 
   return (
     <div className="w-screen h-screen bg-slate-400 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-5">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
-        <Tasks tasks={tasks}
-         onTaskClick={onTaskClick}
-         onDeleteTaskClick={onDeleteTaskClick} />
+        <AddTasks Add={onAddtaskSubmitt} /> 
+
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
       </div>
     </div>
   );
--- src/components/AddTasks.jsx
+++ src/components/AddTasks.jsx
@@ -1,7 +1,35 @@
-export function AddTasks(){
-    return(
-        <div>
-            
-        </div>
-    )
+import { useState } from "react";
+
+export function AddTasks({ Add }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div className="space-y-4 p-6 bg-slate-400 rounded-md flex flex-col">
+      <input
+        type="text"
+        className="border border-slate-200 outline-slate-400 px-4 py-2 rounded-md"
+        placeholder="Digite o título da tarefa"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        className="border border-slate-200 outline-slate-400 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+
+      <button
+        onClick={() => {Add(title, description);
+                        setTitle("");
+                        setDescription("")}
+        } 
+        className="bg-slate-600 rounded-md px-4 py-2 text-white"
+      >
+        Adicionar
+      </button>
+    </div>
+  );
 }
